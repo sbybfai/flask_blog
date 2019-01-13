4bf87a44e7107013e51540b45457942c49da5ae9
sbybfai <sbybfai@gmail.com>
2019-01-13 15:01:05 +0800

    add MAIL_USE_SSL setting

--- config.py
+++ config.py
@@ -6,11 +6,12 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or 'hard to guess string'
     MAIL_SERVER = os.environ.get("MAIL_SERVER", 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get("MAIL_PORT", '587'))
-    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", 'ture').lower() in ['ture', 'on', 1]
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", 'true').lower() in ['true', 'on', 1]
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", 'true').lower() in ['true', 'on', 1]
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     FLASKY_MAIL_SUBJECT_PREFIX = "[Sbybfai's Blog]"
-    FLASKY_MAIL_SENDER = "Sbybfai's Blog <flasky@excample.com>"
+    FLASKY_MAIL_SENDER = MAIL_USERNAME
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
