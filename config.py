64368d314f8d47004f331ee813965b508a3f4b91
sbybfai <sbybfai@gmail.com>
2019-01-13 11:38:09 +0800

    fix names

--- config.py
+++ config.py
@@ -26,11 +26,11 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI") or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URI") or 'sqlite://'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL") or 'sqlite://'
     WTF_CSRF_ENABLED = False
 
 class ProductionConfig(Config):
