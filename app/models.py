9d34c6ed409949ac0d6e9e572f61bda1687259a6
lxd <sbybfai@gmail.com>
2019-04-05 16:08:32 +0800

    add markdown extra

--- app/models.py
+++ app/models.py
@@ -250,12 +250,7 @@ class Post(db.Model):
 
     @staticmethod
     def on_changed_body(target, value, oldvalue, initiator):
-        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
-                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
-        target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format='html'),
-            tags=allowed_tags, strip=True))
+        target.body_html = bleach.linkify(markdown(value, extensions=["extra"]))
 
     def to_json(self):
         json_post = {
